f015bd266093e64543daa0511ecdf4b4fdaad90b
Daniel Walicki <danielrwalicki@gmail.com>
2019-04-07 15:28:14 -0700

    Server changes

--- server/server.js
+++ server/server.js
@@ -16,6 +16,8 @@ const { auth } = require('./middleware/auth');
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.use(express.static('client/build'));
+
 // GET //
 
 app.get('/api/auth',auth,(req,res)=>{
@@ -154,6 +156,13 @@ app.delete('/api/delete_recipe',(req,res)=>{
         res.json(true)
     })
 })
+
+if(process.env.NODE_ENV === 'production'){
+    const path = require('path');
+    app.get('/*', (req,res)=>{
+        res.sendfile(path, resolve(__dirname, '../client', 'build', 'index.html'))
+    })
+}
 
 const port = process.env.PORT || 3001;
 app.listen(port,()=>{
